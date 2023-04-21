a9b21dd09809421100dae673ca3df93b66fbc462
Md Faizul Islam <36614551+mdfaizulislam@users.noreply.github.com>
2023-04-21 13:52:14 +0600

    entry point of the project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+// dependencies
+import { AppController } from './controllers/AppController';
+import { SceneController } from './controllers/SceneController';
+import { Helper } from './generic/Helper';
+
+AppController.initialize(
+    Helper.getScreenWidth(),
+    Helper.getScreenHeight(),
+    0x6495ed
+);
+
+// pass in the screen size to avoid "asking up"
+// AppController.changeScene(SceneController.getLoadingScene());
+SceneController.loadLoadingScene();
