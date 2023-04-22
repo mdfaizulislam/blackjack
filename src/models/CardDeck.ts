c38ebdb100724a4af9e98436b3fdf9166cb5baf7
Md Faizul Islam <36614551+mdfaizulislam@users.noreply.github.com>
2023-04-23 03:32:38 +0600

    added Card Deck Script

---
+++ src/models/CardDeck.ts
@@ -0,0 +1,44 @@
+export class CardDeck {
+    private mCardValues: number[] = [];
+
+    public static createCardDeck(): CardDeck {
+        return new CardDeck();
+    }
+
+    constructor() {
+        this.initCardDeck();
+        this.shuffleDeck(this.mCardValues);
+    }
+
+    private initCardDeck(): void {
+        for (let i = 1; i < 53; i += 1) {
+            this.mCardValues.push(i);
+        }
+    }
+
+    private shuffleDeck(array: number[]) {
+        let currentIndex: number = array.length;
+        let temporaryValue: number;
+        let randomIndex: number;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+    }
+
+    public getNextCard(): number {
+        if (this.mCardValues.length > 0) {
+            let cardValue = this.mCardValues.pop() as number;
+            return cardValue;
+        }
+        throw new Error('Deck is empty');
+    }
+}
