b79ad827087bee6e41f56a3aa92d33aa17cb6218
Md Faizul Islam <36614551+mdfaizulislam@users.noreply.github.com>
2023-04-24 00:44:29 +0600

    added player script

---
+++ src/models/Player.ts
@@ -0,0 +1,52 @@
+import { AppController } from '../controllers/AppController';
+import { Logger } from '../generic/Logger';
+
+export class Player {
+    private mBalance: number = 0;
+    private mLogger: Logger;
+
+    public static createPlayer(): Player {
+        return new Player();
+    }
+    constructor() {
+        this.mBalance = 60;
+        this.mLogger = Logger.createLogger('Player', true);
+    }
+
+    getCurrentMoney(): number {
+        return this.mBalance;
+    }
+
+    public addMoney(money: number): void {
+        if (money < 0) {
+            throw new Error("Can't add negative money to player balance");
+        }
+        this.mBalance += money;
+        this.onBalanceUpdate();
+    }
+
+    public deductMoney(money: number): void {
+        if (this.mBalance - money < 0) {
+            this.mLogger.Error('Donot have enough money to deduct');
+            return;
+        }
+
+        this.mBalance -= money;
+        this.onBalanceUpdate();
+    }
+
+    public hasEnoughMoney(money: number): boolean {
+        return this.mBalance - money >= 0;
+    }
+
+    private onBalanceUpdate(): void {
+        AppController.getPersistantNode()
+            .getBalanceListener()
+            .getAllListerns()
+            .forEach((callback) => {
+                if (callback) {
+                    callback();
+                }
+            });
+    }
+}
