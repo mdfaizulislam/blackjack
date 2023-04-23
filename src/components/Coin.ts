9cc3ea36a9db4667f96cf0371b78ca5bdba51d63
Md Faizul Islam <36614551+mdfaizulislam@users.noreply.github.com>
2023-04-24 00:42:27 +0600

    added chip button

---
+++ src/components/Coin.ts
@@ -0,0 +1,41 @@
+import { Sprite, Text } from 'pixi.js';
+import { Helper } from '../generic/Helper';
+import { Logger } from '../generic/Logger';
+import { Button } from './Button';
+import { AppController } from '../controllers/AppController';
+
+export class Coin extends Button {
+    private mValue: number;
+
+    public static createCoin(value: number) {
+        let card = new Coin(value);
+        return card;
+    }
+
+    constructor(value: number) {
+        super('coinEnabled', 'coinEnabled', 'coinEnabled', 'coinDisabled');
+        this.mValue = value;
+        this.setButtonText(value + '');
+        AppController.getPersistantNode()
+            .getBalanceListener()
+            .addCallbackListener(this.onBalanceUpdate.bind(this));
+    }
+
+    onBalanceUpdate(): void {
+        let isPurchasable = AppController.getPersistantNode()
+            .getPlayer()
+            .hasEnoughMoney(this.mValue);
+        this.setButtonStatus(!isPurchasable);
+    }
+
+    public getCoinValue(): number {
+        this.mLogger.Log('value: ' + this.mValue);
+        return this.mValue;
+    }
+
+    onDisable() {
+        AppController.getPersistantNode()
+            .getBalanceListener()
+            .removeCallbackListener(this.onBalanceUpdate.bind(this));
+    }
+}
